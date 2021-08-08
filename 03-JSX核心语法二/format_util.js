a87c01385a24670d159cb8d6d4027c83191e79a6
zcy22606 <619730086@qq.com>
2021-08-09 00:53:41 +0800

    条件渲染

---
+++ 03-JSX核心语法二/format_util.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-08 21:44:21
+ * @LastEditTime: 2021-08-08 21:45:47
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \React学习\03-JSX核心语法二\format_util.js
+ */
+export function getSizeImage(url, size) {
+  return `${url}param=${size}*${size}`
+}
